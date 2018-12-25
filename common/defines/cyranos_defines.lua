e85f2ac89994e1a624e01ec2d724a8b098bceaab
Cyrano7 <Cyrano7@users.noreply.github.com>
2018-12-25 02:13:36 -0800

    Prevent artifacts from being lost during successful secession

---
+++ common/defines/cyranos_defines.lua
@@ -0,0 +1,3 @@
+--Prevents artifacts from being lost during successful secession
+NDefines.NCharacter.TREASURY_CHANCE_TO_DISAPPEAR_STANDARD = 0	 	-- Chances that artifacts disappear on inheritance reduced from 0.05
+NDefines.NCharacter.TREASURY_CHANCE_TO_DISAPPEAR_NO_HEIR = 0.25		-- Chances that artifacts disappear on inheritance when the dying character doesn't have an heir reduced from 0.5
